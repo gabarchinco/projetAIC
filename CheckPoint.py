99f8fc8315a2722d3ea9838f45a7eaa8481e2232
Ibrahima DIALLO <kouchner.diallo@gmail.com>
2021-03-11 12:00:59 +0100

    Ajout de img pdf_cons.py

--- CheckPoint.py
+++ CheckPoint.py
@@ -1,3 +1,5 @@
+### Importer les modules nécessaires
+
 import os
 import os.path
 import subprocess
@@ -6,13 +8,18 @@ import sys
 
 from colorama import Fore
 from colorama import Style
-from reportlab.pdfgen import canvas
-from reportlab.lib import colors
+# from reportlab.pdfgen import canvas
+# from reportlab.lib import colors
 from textwrap import wrap
-from fpdf import FPDF 
 
 import const as c
 
+
+################### Ecriture de fichier temporaires ###################
+
+def ecrireFichier(fichieraecrire):
+    with open(c.fichierContenu, "w") as f:
+        f.write(fichieraecrire)
 
 ################### Suppression de fichier temporaires ################
 
@@ -23,12 +30,6 @@ def suppressionFichier(fichierasuppr):
     if os.path.exists(fichierasuppr):
         os.remove(fichierasuppr)
         print("Les fichiers temporaires du processus ont été supprimés")
-
-################### Ecriture de fichier temporaires ###################
-
-def ecrireFichier(fichieraecrire):
-    with open(c.fichierContenu, "w") as f:
-        f.write(fichieraecrire)
 
 ################### Générer le rapport en fichier txt #################
 
@@ -349,7 +350,7 @@ def verif_bootloader_pass():
         print()
         print(f"\t {c.com_rub_bootloader_pass1} \t {Style.BRIGHT} [ {Fore.RED} BOOTLOADER EXPOSE{Style.RESET_ALL}{Style.BRIGHT} ]{Style.RESET_ALL}")
 
-# ## Procéder à la réalisation des différnts tests
+### Procéder à la réalisation des différnts tests
 verif_sys_fichier(c.point_test)
 verif_sys_vfat(c.point_test_vfat)
 test_vardir(c.point_test_vardir)
@@ -360,88 +361,9 @@ verif_auth_single_user_mode()
 verif_dev_shm()
 verif_bootloader_pass()
 
-## Processus de génération du rapport en pdf
-
-## Pour le pdf
-fileName = "Rapport d'audit.pdf"
-documentTitle = "Rapport de test de sécurité"
-titleup = "Rapport du test d'audit"
-title = titleup.upper()
-subTitle = "Tests et récommandations"
-image = "cover1.jpg"
-
-
-
-## Processus de génération du rapport en pdf
-### Initier la class PDF
-class PDF(FPDF):
-        # L'entête du pdf
-    def header(self):
-        # Initier la police du document
-        self.set_font('Arial', 'B', 12)
-        # Initier la position du titre et sa couleur
-        w = self.get_string_width(title) + 151
-        self.set_x((210 - w) / 2)
-        # Initier la position du sous-titre et sa couleur
-        self.set_draw_color(0, 80, 180)
-        self.set_fill_color(230, 230, 0)
-        self.set_text_color(220, 50, 50)
-        self.cell(w, 6, title, 1, 1, 'C', 1)
-        # Inserer un saut de ligne
-        self.ln(8)
-
-    # Pied de page du pdf footer
-    def footer(self):
-        # Positionnement à 1.5 cm du b20as de page
-        self.set_y(-15)
-        # Initier la police en Arial italic de taille 8
-        self.set_font('Arial', 'I', 8)
-        # Insere les numéros de page
-        self.cell(0, 10, 'Page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
-
-# Instancier la class PDF
-pdf = PDF()
-pdf.alias_nb_pages()
-# Créer la page
-pdf.add_page() 
-# Définir le style et la marge gauche
-# pdf.set_left_margin(10)
-pdf.set_font('Arial', 'B', 18)
-
-pdf.image(image, 0, 0, h=300, w=210)
-
-pdf.set_font('Arial', 'B', 18)
-pdf.set_left_margin(0)
-pdf.set_draw_color(0, 80, 180)
-pdf.set_fill_color(230, 230, 0)
-pdf.set_text_color(220, 50, 50)
-w = pdf.get_string_width(title) + 123
-pdf.set_x((209 - w) / 2)
-pdf.set_y(10)
-pdf.cell(w, 12, title, 0, 1, 'C', 1)
-pdf.ln(250)
-
-
-pdf.set_font("Arial", size = 12) 
-pdf.set_left_margin(10)
-pdf.set_right_margin(10)
-pdf.cell(190, 5, txt = c.context.upper(), ln = 1, align = 'L') 
-pdf.ln(5)
-pdf.multi_cell(w=190, h=5, txt = c.first_page, align = 'J')
-pdf.ln(220)
-# Insérer le texte dans le doument pdf 
-f = open(c.fichierRapport, "r") 
-for x in f:
-    if "[+]" in x or ("---") in x:
-        pdf.set_left_margin(10)
-        pdf.cell(200, 5, txt = x, ln = 1, align = 'L') 
-    else:
-        pdf.set_left_margin(10)
-        pdf.set_x(10)
-        pdf.multi_cell(200, 5, txt = x, align = 'L') 
-
 ### Générer le rapport des tests en pdf
-pdf.output(fileName) 
+import pdf_gen as pdf_gen
+pdf_gen.generatepdf()
 
 ## Supprimer les fichier temporaires
 suppressionFichier(c.fichierContenu)
---
+++ pdf_cons.py
@@ -0,0 +1,12 @@
+### Les variables du pdf
+
+fileName = "Rapport d'audit.pdf"
+# Titre du document en majuscule
+titleup = "Rapport du test d'audit"
+title = titleup.upper()
+# Sous-tite du document
+subTitle = "Tests et récommandations"
+# Image insérée dans le document
+image = "img/cover.jpg"
+# # Fichier rapport en txt
+# fichierRapport = "rapport.txt"
